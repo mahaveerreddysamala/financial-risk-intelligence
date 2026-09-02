91bae600e52bd500fb2176c8e250f9bdc7fc92e4
mahaveerreddysamala <mahaveerreddysamala21@gmail.com>
2026-09-02 18:49:24 -0500

    feat: export streaming risk scoring helpers

--- src/financial_risk/streaming/__init__.py
+++ src/financial_risk/streaming/__init__.py
@@ -1,5 +1,16 @@
 """Kafka-compatible financial risk event streaming helpers."""
 
 from financial_risk.streaming.events import EventEnvelope, serialize_event
+from financial_risk.streaming.risk_consumer import (
+    RiskScoringResult,
+    score_transaction_event,
+    scoring_result_event,
+)
 
-__all__ = ["EventEnvelope", "serialize_event"]
+__all__ = [
+    "EventEnvelope",
+    "RiskScoringResult",
+    "score_transaction_event",
+    "scoring_result_event",
+    "serialize_event",
+]
