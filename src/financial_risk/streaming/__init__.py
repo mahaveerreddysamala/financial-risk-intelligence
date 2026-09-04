bcc05a92121969d5c9597261a02b7681ab9b8d7f
mahaveerreddysamala <mahaveerreddysamala21@gmail.com>
2026-09-04 16:27:08 -0500

    feat(phase46): export streaming graph enrichment

--- src/financial_risk/streaming/__init__.py
+++ src/financial_risk/streaming/__init__.py
@@ -1,6 +1,7 @@
 """Kafka-compatible financial risk event streaming helpers."""
 
 from financial_risk.streaming.events import EventEnvelope, serialize_event
+from financial_risk.streaming.graph_enrichment import GraphEnrichmentResult, StreamingGraphEnricher
 from financial_risk.streaming.risk_consumer import (
     RiskScoringResult,
     score_transaction_event,
@@ -9,7 +10,9 @@ from financial_risk.streaming.risk_consumer import (
 
 __all__ = [
     "EventEnvelope",
+    "GraphEnrichmentResult",
     "RiskScoringResult",
+    "StreamingGraphEnricher",
     "score_transaction_event",
     "scoring_result_event",
     "serialize_event",
